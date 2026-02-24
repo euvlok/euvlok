a892b44a7202d35a3b07a16f3db8e86cf1252922
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2026-02-24 20:46:58 +0700

    misc stuff

--- hosts/darwin/bigshaq9999/nanachi/system.nix
+++ hosts/darwin/bigshaq9999/nanachi/system.nix
@@ -4,8 +4,6 @@
     keyboard.enableKeyMapping = true;
     defaults.dock = {
       tilesize = 44;
-      orientation = lib.mkForce "left";
-      autohide = lib.mkForce false;
     };
     stateVersion = 5;
   };
