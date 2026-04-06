a047ff4cc740257a380b57d852f558f5941e9216
FlameFlag <github@flameflag.dev>
2026-04-06 17:59:55 +0300

    feat: add claude statusline and claude `settings.json`

---
+++ modules/hm/cli/claude/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, lib, ... }:
+{
+  home.file.".claude/statusline-command.sh" = {
+    source = ./statusline-command.sh;
+    executable = true;
+  };
+
+  home.file.".claude/settings.json".text = builtins.toJSON {
+    statusLine = {
+      type = "command";
+      command = "${lib.getExe' pkgs.bash "bash"} ~/.claude/statusline-command.sh";
+    };
+    enabledPlugins = {
+      "frontend-design@claude-plugins-official" = true;
+      "gopls-lsp@claude-plugins-official" = true;
+      "clangd-lsp@claude-plugins-official" = true;
+      "rust-analyzer-lsp@claude-plugins-official" = true;
+      "swift-lsp@claude-plugins-official" = true;
+    };
+    extraKnownMarketplaces = {
+      "claude-plugins-official" = {
+        source = {
+          source = "git";
+          url = "git@github.com:anthropics/claude-plugins-official.git";
+        };
+      };
+    };
+    effortLevel = "medium";
+    autoUpdatesChannel = "latest";
+    skipDangerousModePermissionPrompt = true;
+  };
+}
