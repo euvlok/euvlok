590514f0979a2674b6adf331ef57cf59de618c9f
lay-by <44959695+lay-by@users.noreply.github.com>
2026-04-08 14:34:44 -0500

    fix: added enable option to claude hm module

--- modules/hm/cli/claude/default.nix
+++ modules/hm/cli/claude/default.nix
@@ -1,32 +1,41 @@
-{ pkgs, lib, ... }:
 {
-  home.file.".claude/statusline-command.sh" = {
-    source = ./statusline-command.sh;
-    executable = true;
-  };
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  options.hm.claude-code.enable = lib.mkEnableOption "claude-code";
 
-  home.file.".claude/settings.json".text = builtins.toJSON {
-    statusLine = {
-      type = "command";
-      command = "${lib.getExe' pkgs.bash "bash"} ~/.claude/statusline-command.sh";
+  config = lib.mkIf config.hm.claude-code.enable {
+    home.file.".claude/statusline-command.sh" = {
+      source = ./statusline-command.sh;
+      executable = true;
     };
-    enabledPlugins = {
-      "frontend-design@claude-plugins-official" = true;
-      "gopls-lsp@claude-plugins-official" = true;
-      "clangd-lsp@claude-plugins-official" = true;
-      "rust-analyzer-lsp@claude-plugins-official" = true;
-      "swift-lsp@claude-plugins-official" = true;
-    };
-    extraKnownMarketplaces = {
-      "claude-plugins-official" = {
-        source = {
-          source = "git";
-          url = "git@github.com:anthropics/claude-plugins-official.git";
+
+    home.file.".claude/settings.json".text = builtins.toJSON {
+      statusLine = {
+        type = "command";
+        command = "${lib.getExe' pkgs.bash "bash"} ~/.claude/statusline-command.sh";
+      };
+      enabledPlugins = {
+        "frontend-design@claude-plugins-official" = true;
+        "gopls-lsp@claude-plugins-official" = true;
+        "clangd-lsp@claude-plugins-official" = true;
+        "rust-analyzer-lsp@claude-plugins-official" = true;
+        "swift-lsp@claude-plugins-official" = true;
+      };
+      extraKnownMarketplaces = {
+        "claude-plugins-official" = {
+          source = {
+            source = "git";
+            url = "git@github.com:anthropics/claude-plugins-official.git";
+          };
         };
       };
+      effortLevel = "medium";
+      autoUpdatesChannel = "latest";
+      skipDangerousModePermissionPrompt = true;
     };
-    effortLevel = "medium";
-    autoUpdatesChannel = "latest";
-    skipDangerousModePermissionPrompt = true;
   };
 }
