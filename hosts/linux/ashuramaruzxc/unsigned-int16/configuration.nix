2e37f932d36a1f532d5a37af0bb167ad892b44ea
FlameFlag <github@flameflag.dev>
2026-04-10 13:41:00 +0300

    feat: add github token for nix

--- hosts/linux/ashuramaruzxc/unsigned-int16/configuration.nix
+++ hosts/linux/ashuramaruzxc/unsigned-int16/configuration.nix
@@ -139,10 +139,18 @@
     serif = [ "Noto Nerd Font" ];
     emoji = [ "Twitter Color Emoji" ];
   };
-  sops.secrets.gh_token = { };
-  sops.secrets.netrc_creds = { };
+  sops.secrets.gh_token = {
+    mode = "0440";
+    group = "users";
+  };
+  sops.secrets.netrc_creds = {
+    mode = "0440";
+    group = "users";
+  };
 
-  nix.settings.access-tokens = config.sops.secrets.gh_token.path;
+  nix.extraOptions = ''
+    !include ${config.sops.secrets.gh_token.path}
+  '';
   nix.settings.netrc-file = config.sops.secrets.netrc_creds.path;
 
   nix.gc.automatic = true;
