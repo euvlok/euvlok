6db0ec9d322321fd67e6c3c82cc6052a5a539eb7
ashuramaruzxc <ashuramaru@tenjin-dk.com>
2026-04-23 15:56:38 +0200

    refactor(ashuramaruzxc): extract shared workstation defaults

--- hosts/linux/ashuramaruzxc/unsigned-int32/configuration.nix
+++ hosts/linux/ashuramaruzxc/unsigned-int32/configuration.nix
@@ -14,6 +14,7 @@
     ../shared/system/desktop.nix
     ../shared/system/nix-credentials.nix
     ../shared/system/pam-security.nix
+    ../shared/system/workstation.nix
     ../shared/system/settings.nix
     ../shared/system/fonts.nix
     ./hardware-configuration.nix
@@ -22,14 +23,6 @@
     ./steam.nix
     ./users.nix
   ];
-
-  environment.localBinInPath = true;
-  environment.sessionVariables = {
-    XDG_DATA_HOME = "\${HOME}/.local/share";
-    XDG_CACHE_HOME = "\${HOME}/.cache";
-    XDG_CONFIG_HOME = "\${HOME}/.config";
-    XDG_DATA_DIRS = [ "\${HOME}/.local/share/.icons" ];
-  };
 
   hardware = {
     gpgSmartcards.enable = true;
@@ -152,43 +145,6 @@
     };
   };
 
-  nixos.locale = {
-    enable = true;
-    timeZone = "Europe/Warsaw";
-    extraLocaleSettings = {
-      LC_MESSAGES = "en_US.UTF-8";
-      LC_MEASUREMENT = "pl_PL.UTF-8";
-      LC_MONETARY = "pl_PL.UTF-8";
-      LC_TIME = "pl_PL.UTF-8";
-      LC_PAPER = "pl_PL.UTF-8";
-      LC_ADDRESS = "pl_PL.UTF-8";
-      LC_TELEPHONE = "pl_PL.UTF-8";
-      LC_NUMERIC = "pl_PL.UTF-8";
-    };
-  };
-  i18n = {
-    supportedLocales = [
-      "en_US.UTF-8/UTF-8"
-      "pl_PL.UTF-8/UTF-8"
-      "all"
-    ];
-    inputMethod = {
-      enable = true;
-      type = "fcitx5";
-      fcitx5 = {
-        waylandFrontend = true;
-        addons = builtins.attrValues {
-          inherit (pkgs) fcitx5-gtk fcitx5-mozc;
-        };
-      };
-    };
-  };
-  fonts.fontconfig.defaultFonts = {
-    monospace = [ "Hack Nerd Font Mono" ];
-    sansSerif = [ "Noto Nerd Font" ];
-    serif = [ "Noto Nerd Font" ];
-    emoji = [ "Twitter Color Emoji" ];
-  };
   virtualisation.oci-containers.containers.FlareSolverr = {
     image = "ghcr.io/flaresolverr/flaresolverr:latest";
     autoStart = true;
