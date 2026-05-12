53846dbe8d45596b7583790e6e058e4373107e56
ashuramaruzxc <ashuramaru@tenjin-dk.com>
2026-05-12 12:04:49 +0200

    feat(nvidia): remove egl hack

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -24,34 +24,6 @@ in
           "NVreg_UsePageAttributeTable=1"
           "NVreg_RegistryDwords=RMUseSwI2c=0x01;RMI2cSpeed=100"
         ];
-      # Credit: https://github.com/NixOS/nixpkgs/issues/202454#issuecomment-1579609974
-      environment.etc."egl/egl_external_platform.d".source =
-        let
-          nvidia_wayland = pkgs.writeText "10_nvidia_wayland.json" ''
-            {
-                "file_format_version" : "1.0.0",
-                "ICD" : {
-                    "library_path" : "${pkgs.unstable.egl-wayland}/lib/libnvidia-egl-wayland.so"
-                }
-            }
-          '';
-          nvidia_gbm = pkgs.writeText "15_nvidia_gbm.json" ''
-            {
-                "file_format_version" : "1.0.0",
-                "ICD" : {
-                    "library_path" : "${config.hardware.nvidia.package}/lib/libnvidia-egl-gbm.so.1"
-                }
-            }
-          '';
-        in
-        lib.mkForce (
-          pkgs.runCommandLocal "nvidia-egl-hack" { } ''
-            mkdir -p "$out"
-            cp ${nvidia_wayland} "$out/10_nvidia_wayland.json"
-            cp ${nvidia_gbm} "$out/15_nvidia_gbm.json"
-          ''
-        );
-
       environment.sessionVariables = {
         __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # without this NOUVEAU may attempt to be used instead
         GBM_BACKEND = "nvidia-drm"; # Required to run the correct GBM backend for NVIDIA GPUs on Wayland
