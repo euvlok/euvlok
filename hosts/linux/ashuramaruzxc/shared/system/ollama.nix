36af0f68f92b351d54f27ccb7dd6959401e06c39
ashuramaruzxc <ashuramaru@tenjin-dk.com>
2026-05-12 12:01:13 +0200

    feat(unsigned-int32): enable ollama service

---
+++ hosts/linux/ashuramaruzxc/shared/system/ollama.nix
@@ -0,0 +1,11 @@
+_: {
+  services.ollama = {
+    enable = true;
+    models = "/Shared/games/models";
+    # loadModels = [
+    # "qwen3.5-9b-hauhaucs-aggresive"
+    # "qwen3.6-35b-hauhaucs-aggressive"
+    # ];
+    # syncModels = true;
+  };
+}
