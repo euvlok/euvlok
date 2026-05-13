b546fa6f9b5ee48e360abb2675bdac56e8d7308c
ashuramaruzxc <ashuramaru@tenjin-dk.com>
2026-05-13 14:08:54 +0200

    fix(devenv): remove explicit shell root

--- flake-modules/dev-shell.nix
+++ flake-modules/dev-shell.nix
@@ -20,7 +20,6 @@
 
       devenv.shells.default = {
         name = "euvlok development shell";
-        devenv.root = toString ../.;
         languages = {
           nix.enable = true;
           shell.enable = true;
