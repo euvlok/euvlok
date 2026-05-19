fc32bda0e21e58166ea88b496a245dbdd653e88e
ashuramaruzxc <ashuramaru@tenjin-dk.com>
2026-05-19 13:35:10 +0200

    fix(hm): inherit gpu nixpkgs defaults

--- modules/hm/nixpkgs.nix
+++ modules/hm/nixpkgs.nix
@@ -1,1 +1,13 @@
-_: { imports = [ ../cross/nixpkgs.nix ]; }
+{
+  lib,
+  osConfig ? null,
+  ...
+}:
+{
+  imports = [ ../cross/nixpkgs.nix ];
+
+  config = lib.mkIf (osConfig != null && osConfig ? nixos) {
+    nixpkgs.config.cudaSupport = lib.mkDefault (osConfig.nixos.nvidia.enable or false);
+    nixpkgs.config.rocmSupport = lib.mkDefault (osConfig.nixos.amd.enable or false);
+  };
+}
