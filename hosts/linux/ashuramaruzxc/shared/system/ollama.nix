a2e0478d29b94edf154fc0b002ef0453f59fbf13
ashuramaruzxc <ashuramaru@tenjin-dk.com>
2026-07-01 15:48:19 +0200

    chore(ollama): use default model settings

--- hosts/linux/ashuramaruzxc/shared/system/ollama.nix
+++ hosts/linux/ashuramaruzxc/shared/system/ollama.nix
@@ -1,11 +1,5 @@
 _: {
   services.ollama = {
     enable = true;
-    models = "/Shared/games/models";
-    # loadModels = [
-    # "qwen3.5-9b-hauhaucs-aggresive"
-    # "qwen3.6-35b-hauhaucs-aggressive"
-    # ];
-    # syncModels = true;
   };
 }
