4a2d49359457e4ffb122ba0d8e9ff5d8a393ccc3
4evy <git@evy.pink>
2026-09-22 14:40:00 +0300

    ssh: share Ashuramaru authorized keys across hosts

--- hosts/linux/ashuramaruzxc/unsigned-int64/users.nix
+++ hosts/linux/ashuramaruzxc/unsigned-int64/users.nix
@@ -52,19 +52,7 @@
         "podman"
         "minecraft"
       ];
-      openssh.authorizedKeys.keys = [
-        "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBNR1p1OviZgAkv5xQ10NTLOusPT8pQUG2qCTpO3AhmxaZM2mWNePVNqPnjxNHjWN+a/FcZ5on74QZQJtwXI5m80AAAAOc3NoOnJlbW90ZS1kc2E= email:ashuramaru@tenjin-dk.com id:ashuramaru@unsigned-int32"
-        ### --- ecdsa-sk --- ###
-        ### --- ecdsa-sk_bio --- ###
-        "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBFdzdMIdu/bKlIkGx1tCf1sL65NwrmpvBZQ+nSbKknbGHdrXv33mMzLVUsCGUaUxmeYcCULNNtSb0kvgDjRlcgIAAAAOc3NoOnJlbW90ZS1kc2E= email:ashuramaru@tenjin-dk.com id:ashuramaru@unsigned-int32"
-        ### --- ecdsa-sk_bio --- ###
-        ### --- ed25519-sk --- ###
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKNF4qCh49NCn6DUnzOCJ3ixzLyhPCCcr6jtRfQdprQLAAAACnNzaDpyZW1vdGU= email:ashuramaru@tenjin-dk.com id:ashuramaru@unsigned-int32"
-        ### --- ed25519-sk --- ###
-        ### --- ed25519-sk_bio --- ###
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEF0v+eyeOEcrLwo3loXYt9JHeAEWt1oC2AHh+bZP9b0AAAACnNzaDpyZW1vdGU= email:ashuramaru@tenjin-dk.com id:ashuramaru@unsigned-int32"
-        ### --- ed25519-sk_bio --- ###
-      ];
+      openssh.authorizedKeys.keys = import ../../../keys/ashuramaru.nix;
       shell = pkgs.zsh;
     };
     fumono = {
